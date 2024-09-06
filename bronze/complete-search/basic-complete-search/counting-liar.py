b9856e8db8a20d4fd2155f0e4628e5e7ccf42809
Aditya Kumar <neelaadityakumar@gmail.com>
2024-09-06 19:53:14 +0530

    add liar

--- bronze/complete-search/basic-complete-search/counting-liar.py
+++ bronze/complete-search/basic-complete-search/counting-liar.py
@@ -1,1 +1,34 @@
 # https://usaco.org/index.php?page=viewproblem2&cpid=1228
+
+
+import sys
+sys.setrecursionlimit(10**9)
+
+external=0
+useLocal=1
+if external==1:
+    if useLocal==1:
+        sys.stdin = open("/Users/aditya.kumar/Desktop/code/_test/test.in", "r")
+        sys.stdout = open("/Users/aditya.kumar/Desktop/code/_test/test.out", "w")
+    else:
+        sys.stdin = open("diamond.in", "r")
+        sys.stdout = open("diamond.out", "w")
+MOD = 10**9 + 7
+
+import math
+
+pos = []
+for i in range(int(input())):
+    lg, val = input().split()
+    pos.append([lg, int(val)])
+
+ans = math.inf
+for _, val in pos:
+    liars = 0
+    for lg, v in pos:
+        if lg == 'G':
+            if v > val: liars += 1
+        elif lg == 'L':
+            if v < val: liars += 1
+    ans = min(ans, liars)
+print(ans)
