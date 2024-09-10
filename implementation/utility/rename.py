0b956141d557b76c239923d8edd853f4ac19faee
Aditya Kumar <neelaadityakumar@gmail.com>
2024-09-10 22:50:00 +0530

    aa

--- implementation/utility/rename.py
+++ implementation/utility/rename.py
@@ -2,6 +2,6 @@ import sys
 sys.stdin = open("/Users/aditya.kumar/Desktop/code/_test/test.in", "r")
 sys.stdout = open("/Users/aditya.kumar/Desktop/code/_test/test.out", "w")
 
-name="Year of the Cow"
+name="Don't Be Last"
 
 print(name.lower().replace(" ", "-"))
