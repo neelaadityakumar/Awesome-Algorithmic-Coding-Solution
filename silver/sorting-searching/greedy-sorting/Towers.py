5e4c9e8ebc2e819f39b635b6124706252ad86d09
Aditya Kumar <neelaadityakumar@gmail.com>
2024-09-12 23:58:38 +0530

    add

---
+++ silver/sorting-searching/greedy-sorting/Towers.py
@@ -0,0 +1,19 @@
+import sys
+import statistics
+
+external = 1
+useLocal = 1
+if external == 1:
+    if useLocal == 1:
+        sys.stdin = open("/Users/aditya.kumar/Desktop/code/_test/test.in", "r")
+        sys.stdout = open(
+            "/Users/aditya.kumar/Desktop/code/_test/test.out", "w")
+    else:
+        sys.stdin = open("pairup.in", "r")
+        sys.stdout = open("pairup.out", "w")
+N = int(input())
+nums = [int(i) for i in input().split()]
+m = statistics.median(nums)
+
+
+print(int(sum([abs(i-m) for i in nums])))
