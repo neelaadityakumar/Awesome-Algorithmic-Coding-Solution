06fa7afce5b6dfe943a46d27cc48ec77b0afa8e7
Aditya Kumar <neelaadityakumar@gmail.com>
2024-09-15 00:37:36 +0530

    add

--- cses-solution/Sorting and Searching/Distinct Numbers.py
+++
@@ -1,9 +0,0 @@
-# https://cses.fi/problemset/task/1621
-n = int(input())
-arr = sorted([int(i) for i in input().split()])
-
-ans = min(1, n)
-
-for i in range(1, n):
-    if arr[i] != arr[i-1]:
-        ans += 1
--- usaco/silver/prefix-sum/Subarray-Divisibility.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-arr = list(map(int, input().split()))
-freq = [0 for _ in range(n)]
-ans, cur, freq[0] = 0, 0, 1
-for x in arr:
-    cur = (n + (cur + x) % n) % n
-    ans += freq[cur]
-    freq[cur] += 1
-print(ans)
