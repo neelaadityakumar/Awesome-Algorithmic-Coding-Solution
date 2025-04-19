1bb184cb87ea8c10f58205da5cca7ba48af1597a
Aditya Kumar <neelaadityakumar@gmail.com>
2025-04-19 11:40:17 +0530

    add

---
+++ rename.py
@@ -0,0 +1,7 @@
+import sys
+sys.stdin = open("/Users/aditya.kumar/Desktop/code/_test/test.in", "r")
+sys.stdout = open("/Users/aditya.kumar/Desktop/code/_test/test.out", "w")
+
+name="Hoof Paper Scissors"
+
+print(name.lower().replace(" ", "-"))
