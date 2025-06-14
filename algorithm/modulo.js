5b6edd2fbb03159c1f84529f1291eab68cb43a1c
neelaadityakumar <neelaadityakumar@gmail.com>
2025-06-14 15:25:19 +0530

    add

--- algorithm/modulo.js
+++ algorithm/modulo.js
@@ -13,6 +13,15 @@ function modPow(base, power, mod) {
 
   return result;
 }
+
+function modPow2(base, power, mod) {
+  if (power == 0) return 1 % mod;
+  let result = power(base, power / 2, mod);
+  result = (result * result) % mod;
+  if (power % 2 == 1) result = (result * base) % mod;
+  return result;
+}
+
 //Fermat little theorem ,mode inverse
 // 1/2%mod= 2**-1%mod= modInverse(2,mod)
 function modInverse(base, mod) {
